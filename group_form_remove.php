3775540249d4e8effe83267ddb158c77b8065c3b
Aman Deep Singh <amandeep.singh2016@vitstudent.ac.in>
2017-07-05 19:47:13 +0530

    Update group_form_remove.php

--- group_form_remove.php
+++ group_form_remove.php
@@ -11,6 +11,24 @@
 			echo group_remove($gid, $uname, $gmembers);
 	}
 ?>
+<style>
+h1,h2,h3,h4,h5,h6{
+font-family:ariel
+}
+body::-webkit-scrollbar {
+    width: 0.5em;
+	background-color:transparent;
+}
+ 
+body::-webkit-scrollbar-track {
+    
+}
+ 
+body::-webkit-scrollbar-thumb {
+  background-color: black;
+  
+}
+</style>
 <html>
 	<head>
 		<title>Group Details</title>
